d1aff292fdefe436a7c3b8be7dce54b26a10a23d
imran-hossen-sakil <imranhossenf485@gmail.com>
2024-03-15 23:36:23 +0600

    Flight_booking_Website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className="">
+        <h1 className="text-blue-800 text-4xl text-center mt-6">Hey React js Developer</h1>
+      </div>
+  )
+}
+
+export default App
---
+++ src/component/Top_navbar_section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Top_nav() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Top_nav
