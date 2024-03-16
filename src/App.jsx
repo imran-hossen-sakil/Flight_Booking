89d5a4b3f6f1bf7d7e76ef9867a05acbb01fac1e
imran-hossen-sakil <imranhossenf485@gmail.com>
2024-03-16 10:06:58 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Footer_section from './component/Footer_section'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,10 @@ function App() {
   return (
       <div className="">
         <h1 className="text-blue-800 font-bold text-4xl text-center mt-11">Hey React js Developer</h1>
+
+        < Footer_section />
       </div>
+      
   )
 }
 
--- src/component/Footer_section.jsx
+++ src/component/Footer_section.jsx
@@ -2,9 +2,117 @@ import React from 'react'
 
 function Footer_section() {
   return (
-    <div>
-      
-    </div>
+          <div className="flex items-end w-full min-h-screen bg-white">
+
+            <footer className="w-full text-gray-700 bg-gray-100 body-font">
+                <div
+                    className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+                    <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+                        <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                            <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">CONTACT</h2>
+                            <nav className="mb-10 list-none">
+                                <li className="mt-1">
+                                  <label htmlFor="">Company Name : </label>
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Online Flight Bookin</a>
+                                </li>
+                                <li className="mt-1">
+                                  <label htmlFor="">Email : </label>
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">demo@gmail.com</a>
+                                </li>
+                                <li className="mt-1">
+                                  <label htmlFor="">Number :</label>
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">+09217387632</a>
+                                </li>
+                            
+                            </nav>
+                            <p className="mt-1">
+
+                            <h2 className="mb-3 text-2xl  font-bold tracking-widest text-gray-900 uppercase title-font">Logo</h2>
+                            </p>
+                        </div>
+                        <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                            <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">EXPLORE</h2>
+                            <nav className="mb-10 list-none">
+                                <li className="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                                <li className="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                                <li className="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                                <li className="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                            <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">HELP CENTER
+                            </h2>
+                            <nav className="mb-10 list-none">
+                                <li className="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                                <li class="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                                <li class="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                                <li className="mt-1">
+                                    <a className="text-gray-500 cursor-pointer hover:text-gray-900">Lorem, ipsum.lorem </a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
+                        <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
+                        
+                        <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">ACCEPTED PAYMENTS</h2>
+                        </a>
+                        <div className="mt-4">
+                            <span className="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
+                                <img class="h-auto max-w-full" src="/assets/Payment_image.png" alt="Payment_image" />
+                                <a className="ml-3 mt-40 text-gray-500 cursor-pointer hover:text-gray-700"><br /><br /><br />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                                </svg> 
+                                </a>
+                                <a className="ml-3 mt-40 text-gray-500 cursor-pointer hover:text-gray-700"><br /><br /><br />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                                </svg>
+                                </a>
+                                <a className="ml-3 mt-40 text-gray-500 cursor-pointer hover:text-gray-700"><br /><br /><br />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                                </svg>
+                                </a>
+                                <a className="ml-3 mt-40 text-gray-500 cursor-pointer hover:text-gray-700"><br /><br /><br />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                                </svg>
+                                </a>
+                                <a className="ml-3 mt-40 text-gray-500 cursor-pointer hover:text-gray-700"><br /><br /><br />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                                </svg>
+                                </a>
+                                
+                                
+
+                            </span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="bg-blue-800 w-80 mx-auto rounded-tl-full rounded-tr-full">
+                    <div className="container px-2 py-2 mx-auto">
+                        <p className="text-sm text-white capitalize xl:text-center">© 2024 All rights reserved </p>
+                    </div>
+                </div>
+            </footer>
+          </div>
   )
 }
 
