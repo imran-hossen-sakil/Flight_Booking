e35f16d9429d1003e8f705588f8932740186503c
imran-hossen-sakil <imranhossenf485@gmail.com>
2024-03-18 11:05:41 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Best_travel_section from './component/Best_travel_section'
 import Top_navbar_section from './component/Top_navbar_section'
 import Navbar_section from './component/Navbar_section'
 import Hero_section from './component/Hero_section';
-import Travel_Support_section from './component/Travel_Support_section'
 import Registrar_section from './component/Registrar_section'
 import Group_searching_section from './component/Group_searching_section'
-import Login_section from './component/Login_section'
+import Travel_memories_section from './component/Travel_memories_section'
+
 
 
 
@@ -22,9 +22,9 @@ function App() {
         < Navbar_section />
         < Hero_section />
         < Group_searching_section />
-        < Travel_Support_section />
         < Best_travel_section />
         < Registrar_section />
+        < Travel_memories_section />
 
 
 
