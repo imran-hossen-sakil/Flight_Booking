4325b2f87ed04340041c60f175e52b6e27106b21
imran-hossen-sakil <imranhossenf485@gmail.com>
2024-03-19 15:08:55 +0600

    #

--- src/component/Footer_section.jsx
+++ src/component/Footer_section.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Footer_section() {
   return (
           <div className="flex items-end w-full min-h-screen bg-white">
-
             <footer className="w-full text-gray-700 bg-gray-100 body-font">
                 <div
                     className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
@@ -23,7 +22,6 @@ function Footer_section() {
                                   <label htmlFor="">Number :</label>
                                     <a className="text-gray-500 cursor-pointer hover:text-gray-900">+09217387632</a>
                                 </li>
-                            
                             </nav>
                             <p className="mt-1">
 
@@ -67,7 +65,6 @@ function Footer_section() {
                         </div>
                         <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
                         <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
-                        
                         <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">ACCEPTED PAYMENTS</h2>
                         </a>
                         <div className="mt-4">
@@ -98,9 +95,6 @@ function Footer_section() {
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
                                 </svg>
                                 </a>
-                                
-                                
-
                             </span>
                         </div>
                     </div>
@@ -115,5 +109,4 @@ function Footer_section() {
           </div>
   )
 }
-
 export default Footer_section
