8e937744a7c029530c3f5bc43b4a6e239e92187a
imran-hossen-sakil <imranhossenf485@gmail.com>
2024-03-31 11:51:31 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-import Navbar_section from './component/Navbar_section'
-import Group_searching_section from './component/Group_searching_section';
-import Hero_section from './component/Hero_section';
-import Travel_Support_section from './component/Travel_Support_section';
-import Travel_memories_section from './component/Travel_memories_section';
-import Unaccompanied_section from './component/Unaccompanied_section';
-import Best_travel_section from './component/Best_travel_section';
-import Footer_section from './component/Footer_section';
+
 
 
 
@@ -18,17 +11,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <>
-        < Navbar_section />
-        < Hero_section />
-        < Group_searching_section />
-        < Travel_Support_section />
-        < Travel_memories_section />
-        < Unaccompanied_section />
-        < Best_travel_section />
-        < Footer_section />
-      </>
-      
+    <>
+
+    </>    
   )
 }
 
--- src/component/Best_travel_section.jsx
+++ src/component/Best_travel_section.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Best_travel_section() {
   return (
     <div>
-    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+    <ul role="list" className="grid grid-cols-1 gap-6 max-w-screen-xl	mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
       <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-red-500 text-center shadow">
         <div className="flex flex-1 flex-col p-8">
-          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=yQ-kKmUAAAAJ&citpid=1" alt="" />
+          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://avatars.githubusercontent.com/u/153409370?v=4" alt="" />
           <h3 className="mt-6 text-sm font-medium text-gray-900">Najib Gafar</h3>
           <dl className="mt-1 flex flex-grow flex-col justify-between">
             <dt className="sr-only">Title</dt>
@@ -39,9 +39,9 @@ function Best_travel_section() {
           </div>
         </div>
       </li>
-      <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-red-500 text-center shadow">
+      <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-lime-700 text-center shadow">
         <div className="flex flex-1 flex-col p-8">
-          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=yQ-kKmUAAAAJ&citpid=1" alt="" />
+          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://avatars.githubusercontent.com/u/153409370?v=4" alt="" />
           <h3 className="mt-6 text-sm font-medium text-gray-900">Najib Gafar</h3>
           <dl className="mt-1 flex flex-grow flex-col justify-between">
             <dt className="sr-only">Title</dt>
@@ -74,9 +74,9 @@ function Best_travel_section() {
           </div>
         </div>
       </li>
-      <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-red-500 text-center shadow">
+      <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-cyan-900 text-center shadow">
         <div className="flex flex-1 flex-col p-8">
-          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=yQ-kKmUAAAAJ&citpid=1" alt="" />
+          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://avatars.githubusercontent.com/u/153409370?v=4" alt="" />
           <h3 className="mt-6 text-sm font-medium text-gray-900">Najib Gafar</h3>
           <dl className="mt-1 flex flex-grow flex-col justify-between">
             <dt className="sr-only">Title</dt>
@@ -109,9 +109,9 @@ function Best_travel_section() {
           </div>
         </div>
       </li>
-    <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-cyan-500 text-center shadow">
+      <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-cyan-500 text-center shadow">
         <div className="flex flex-1 flex-col p-8">
-          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=yQ-kKmUAAAAJ&citpid=1" alt="" />
+          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://avatars.githubusercontent.com/u/153409370?v=4" alt="" />
           <h3 className="mt-6 text-sm font-medium text-gray-900">Najib Gafar</h3>
           <dl className="mt-1 flex flex-grow flex-col justify-between">
             <dt className="sr-only">Title</dt>
@@ -146,7 +146,7 @@ function Best_travel_section() {
       </li>
       <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-orange-500 text-center shadow">
         <div className="flex flex-1 flex-col p-8">
-          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=yQ-kKmUAAAAJ&citpid=1" alt="" />
+          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://avatars.githubusercontent.com/u/153409370?v=4" alt="" />
           <h3 className="mt-6 text-sm font-medium text-gray-900">Najib Gafar</h3>
           <dl className="mt-1 flex flex-grow flex-col justify-between">
             <dt className="sr-only">Title</dt>
@@ -181,7 +181,7 @@ function Best_travel_section() {
       </li>
       <li className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-pink-500 text-center shadow">
         <div className="flex flex-1 flex-col p-8">
-          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://scholar.googleusercontent.com/citations?view_op=view_photo&user=yQ-kKmUAAAAJ&citpid=1" alt="" />
+          <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src="https://avatars.githubusercontent.com/u/153409370?v=4" alt="" />
           <h3 className="mt-6 text-sm font-medium text-gray-900">Najib Gafar</h3>
           <dl className="mt-1 flex flex-grow flex-col justify-between">
             <dt className="sr-only">Title</dt>
