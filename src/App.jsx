88be6ec39df889b5d84453fe182bb31b77d0dc8b
imran-hossen-sakil <imranhossenf485@gmail.com>
2024-04-03 13:40:34 +0600

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar_section from './component/user_common/Navbar_section'
+import Hero_section from './component/user_common/Hero_section';
+import Contact_section from './component/Contact_section';
+import Error_page from './component/Error_page';
+import Registrar_section from './component/Registrar_section';
 
 
 
@@ -15,6 +19,10 @@ function App() {
   return (
     <>
       < Navbar_section />
+      < Hero_section />
+      < Contact_section />
+      < Error_page />
+      < Registrar_section />
 
     </>    
   )
